ace9047988a5ed4ac88e9d6d5fe6ced82bb6d0f8
MikleStep <raiving_rabbid@mail.ru>
2017-02-19 17:36:44 +0300

    Версия 1.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,70 @@
-var openFeedback=document.querySelector(".feedback-btn"),feedbackPopup=document.querySelector(".feedback-pop-up"),closeFeedback=document.querySelector(".feedback-close"),openMap=document.querySelector(".map"),mapPopup=document.querySelector(".map-pop-up"),closeMap=document.querySelector(".close-map"),openCheckout=document.querySelector(".buy"),checkoutWindow=document.querySelector(".pop-up"),closeCheckout=document.querySelector(".pop-up-close"),continueShoping=document.querySelector(".continue"),openCheckout=document.querySelector(".buy"),checkoutWindow=document.querySelector(".pop-up"),closeCheckout=document.querySelector(".pop-up-close"),continueShoping=document.querySelector(".continue");openFeedback.addEventListener("click",function(a){a.preventDefault(),feedbackPopup.classList.add("feedback-show"),feedbackPopup.classList.add("animation-open"),feedbackPopup.classList.remove("animation-close")}),closeFeedback.addEventListener("click",function(a){a.preventDefault(),feedbackPopup.classList.add("animation-close"),feedbackPopup.classList.remove("feedback-show"),feedbackPopup.classList.remove("animation-open")}),openMap.addEventListener("click",function(a){a.preventDefault(),mapPopup.classList.add("map-show"),mapPopup.classList.add("animation-open"),mapPopup.classList.remove("animation-close")}),closeMap.addEventListener("click",function(a){function b(){mapPopup.classList.remove("map-show")}a.preventDefault(),mapPopup.classList.add("animation-close"),setTomeout(b,1e3),mapPopup.classList.remove("animation-open")}),openCheckout.addEventListener("click",function(a){a.preventDefault(),checkoutWindow.classList.add("checkout-show"),checkoutWindow.classList.add("animation-open"),checkoutWindow.classList.remove("animation-close")}),closeCheckout.addEventListener("click",function(a){a.preventDefault(),checkoutWindow.classList.remove("checkout-show"),checkoutWindow.classList.remove("animation-open"),checkoutWindow.classList.add("animation-close")}),continueShoping.addEventListener("click",function(a){a.preventDefault(),checkoutWindow.classList.remove("checkout-show"),checkoutWindow.classList.remove("animation-open"),checkoutWindow.classList.add("animation-close")}),openCheckout.addEventListener("click",function(a){a.preventDefault(),checkoutWindow.classList.add("checkout-show")}),closeCheckout.addEventListener("click",function(a){a.preventDefault(),checkoutWindow.classList.remove("checkout-show")}),continueShoping.addEventListener("click",function(a){a.preventDefault(),checkoutWindow.classList.remove("checkout-show")});
+var openFeedback = document.querySelector(".feedback-btn");
+var feedbackPopup = document.querySelector(".feedback-pop-up");
+var closeFeedback = document.querySelector(".feedback-close");
+var openMap = document.querySelector(".map");
+var mapPopup = document.querySelector(".map-pop-up");
+var closeMap = document.querySelector(".close-map");
+var openCheckout = document.querySelectorAll(".buy");
+var checkoutWindow = document.querySelector(".pop-up");
+var closeCheckout = document. querySelector(".pop-up-close");
+var continueShoping = document.querySelector(".continue");
+
+
+openFeedback.addEventListener("click", function(event) {
+	event.preventDefault();
+	feedbackPopup.classList.add("feedback-show");
+	feedbackPopup.classList.add("animation-open");
+	feedbackPopup.classList.remove("animation-close");
+});
+
+closeFeedback.addEventListener("click", function(event){
+	event.preventDefault();
+	feedbackPopup.classList.add("animation-close");
+	function timer(){
+		feedbackPopup.classList.remove("feedback-show");
+	}
+	setTimeout(timer, 300);
+	feedbackPopup.classList.remove("animation-open");        
+});
+
+openMap.addEventListener("click", function(event){
+	event.preventDefault();
+	mapPopup.classList.add("map-show");
+	mapPopup.classList.add("animation-open");
+	mapPopup.classList.remove("animation-close");
+});
+
+closeMap.addEventListener("click", function(event){
+	event.preventDefault();
+	mapPopup.classList.add("animation-close");
+	function timer(){
+		mapPopup.classList.remove("map-show");
+	}
+	setTimeout(timer, 300);
+	mapPopup.classList.remove("animation-open");     
+});
+for (var i = 0; i < openCheckout.length; i++){
+	openCheckout[i].addEventListener("click", function(event){
+		event.preventDefault();
+		checkoutWindow.classList.add("checkout-show");
+		checkoutWindow.classList.add("animation-open");
+		checkoutWindow.classList.remove("animation-close");
+	});
+}
+
+closeCheckout.addEventListener("click", function(event){
+	event.preventDefault();
+	checkoutWindow.classList.remove("checkout-show");
+	function timer(){
+		checkoutWindow.classList.remove("animation-open");
+	}
+	setTimeout(timer, 300);
+	checkoutWindow.classList.add("animation-close");
+});
+
+continueShoping.addEventListener("click", function(event){
+	event.preventDefault();
+	checkoutWindow.classList.remove("checkout-show");
+	checkoutWindow.classList.remove("animation-open");
+	checkoutWindow.classList.add("animation-close");
+});
